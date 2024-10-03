06201062c31fec5a57484d22067ff4496028c435
velsabari <velsabari66@gmail.com>
2024-10-04 00:55:36 +0530

    project basic setup completed

---
+++ client/src/components/Calculater.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Calculater = () => {
+  return (
+    <>
+      <div className='border-2 border-black w-96 h-auto shadow-lg rounded-lg bg-white'>
+        <div className='flex justify-center'>
+            <h1 className='font-bold text-2xl'>CGPA Calculater</h1>
+            <input type="text" placeholder=''/>
+        </div>
+
+      </div>
+    </>
+  )
+}
+
+export default Calculater
--- server/index.js
+++ server/index.js
@@ -1,1 +1,11 @@
 const express=require("express");
+const connectDB = require("./config/Database");
+const dotenv=require("dotenv");
+dotenv.config();
+
+const app=express();
+connectDB();
+
+const port=process.env.PORT || 5000;
+
+app.listen((port,()=>console.log(`server is running on : ${port}`)));
